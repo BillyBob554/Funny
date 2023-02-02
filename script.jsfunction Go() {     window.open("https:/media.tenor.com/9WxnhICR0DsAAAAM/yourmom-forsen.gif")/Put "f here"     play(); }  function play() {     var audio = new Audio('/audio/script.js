8b6b96e78003e5f19cf64aeb5883467318bf6606
BillyBob554 <110690423+BillyBob554@users.noreply.github.com>
2023-02-02 10:27:59 -0500

    Create script.js

---
+++ script.jsfunction Go() {     window.open("https:/media.tenor.com/9WxnhICR0DsAAAAM/yourmom-forsen.gif")/Put "f here"     play(); }  function play() {     var audio = new Audio('/audio/script.js
@@ -0,0 +1,9 @@
+function Go() {
+    window.open("https://media.tenor.com/9WxnhICR0DsAAAAM/yourmom-forsen.gif")// Put "f here"
+    play();
+}
+
+function play() {
+    var audio = new Audio('/audio/audio.mp3');
+    audio.play();
+  }
