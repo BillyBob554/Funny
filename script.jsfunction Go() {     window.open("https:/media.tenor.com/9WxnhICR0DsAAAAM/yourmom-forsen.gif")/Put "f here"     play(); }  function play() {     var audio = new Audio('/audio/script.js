03d2711ea05a5fafa045a108c8ead5d552cc3c54
BillyBob554 <110690423+BillyBob554@users.noreply.github.com>
2023-02-02 10:28:38 -0500

    Delete script.jsfunction Go() {     window.open("https:/media.tenor.com/9WxnhICR0DsAAAAM/yourmom-forsen.gif")/Put "f here"     play(); }  function play() {     var audio = new Audio('/audio directory

--- script.jsfunction Go() {     window.open("https:/media.tenor.com/9WxnhICR0DsAAAAM/yourmom-forsen.gif")/Put "f here"     play(); }  function play() {     var audio = new Audio('/audio/script.js
+++
@@ -1,9 +0,0 @@
-function Go() {
-    window.open("https://media.tenor.com/9WxnhICR0DsAAAAM/yourmom-forsen.gif")// Put "f here"
-    play();
-}
-
-function play() {
-    var audio = new Audio('/audio/audio.mp3');
-    audio.play();
-  }
